4d9d42c39fc80c57825ac898971d0f8cdf87ceed
malarharish007@gmail.com <malarharish@gmail.com>
2025-08-18 14:32:24 +0530

    finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Create Next App',
+  title: 'Harish S Portfolio',
   description: 'Full Stack Developer | MERN | AI & DS',
 }
 
